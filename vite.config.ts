49b616e4edafc09ea572129e5d8e90efbff5f8bc
Richard North <rich.north@gmail.com>
2024-11-11 18:41:01 +0000

    Update sitemap dynamically

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import {defineConfig} from 'vite'
+import {svelte} from '@sveltejs/vite-plugin-svelte'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+    plugins: [
+        svelte(),
+        Sitemap({
+            hostname: 'https://euston.wtf',
+        })
+    ],
 })
